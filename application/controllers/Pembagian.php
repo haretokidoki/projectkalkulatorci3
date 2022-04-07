6494b551a2cf0b93c92dee9080ce9430c0530874
lanjar17 <lanjar17@gmail.com>
2022-04-07 11:37:24 +0700

    fix error saat tidak ada input

--- application/controllers/Pembagian.php
+++ application/controllers/Pembagian.php
@@ -6,11 +6,11 @@ class Pembagian extends CI_Controller
 
     public function index()
     {
-        if($this->input->post() != null){
-            $nilai = $this->input->post();
-            $data ['a'] = $nilai['nilaia'];
-            $data ['b'] = $nilai['nilaib'];
-            $data ['c'] = $data['a']/$data['b'];
+        $nilai = $this->input->post();
+        if ($this->input->post() != null && is_numeric($nilai['nilaia']) && is_numeric($nilai['nilaib'])) {
+            $data['a'] = $nilai['nilaia'];
+            $data['b'] = $nilai['nilaib'];
+            $data['c'] = $data['a'] / $data['b'];
         } else {
             $data['a'] = "";
             $data['b'] = "";
