b38960b735df7713c0b4540ff7b9e1da74d75738
lanjar17 <lanjar17@gmail.com>
2022-04-07 14:42:49 +0700

    add bootstrap

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,16 +1,30 @@
 <?php  ?>
 <!DOCTYPE html>
 <html>
+
 <head>
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" />
 	<title>Dashboard</title>
 </head>
+
 <body>
-	<h1>Kalkulator</h1>
-	<ul>
-		<li><a href="penjumlahan">Penjumlahan</a><br></li>
-		<li><a href="pengurangan">Pengurangan</a><br></li>
-		<li><a href="perkalian">Perkalian</a><br></li>
-		<li><a href="pembagian">Pembagian</a></li>
-	</ul>
+	<div class="container">
+		<div class="card">
+			<div class="card-header">
+				<h1 align="center">KALKULATOR</h1>
+			</div>
+			<div class="card-body">
+				<ul class="list-group">
+					<li class="list-group-item list-group-item-primary"><a href="penjumlahan">Penjumlahan</a></li>
+					<li class="list-group-item list-group-item-primary"><a href="pengurangan">Pengurangan</a></li>
+					<li class="list-group-item list-group-item-secondary"><a href="perkalian">Perkalian</a></li>
+					<li class="list-group-item list-group-item-success"><a href="pembagian">Pembagian</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<script src="<?php echo base_url('assets/js/bootstrap.bundle.min.js') ?>"></script>
 </body>
+
 </html>
